bd7edaf7b4a479bcf8868eae49ff5ac098ef8710
GVAmaresh <eshama944@gmail.com>
2024-04-26 16:52:14 +0530

    ini

--- comparator/compare_model/compare.py
+++ comparator/compare_model/compare.py
@@ -3,11 +3,10 @@ import torch
 import torch.nn.functional as F
 
 
-# Mean Pooling - Take attention mask into account for correct averaging
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[
         0
-    ]  # First element of model_output contains all token embeddings
+    ] 
     input_mask_expanded = (
         attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     )
